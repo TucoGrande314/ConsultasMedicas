573bae933e1707347e624ea337159c292915fff3
u14187 <u14187@CLAUDIO39>
2017-11-16 12:02:30 -0200

    consulta finalizada

--- PPIII/AgendaMedica/DefaultMedico.aspx.cs
+++ PPIII/AgendaMedica/DefaultMedico.aspx.cs
@@ -10,7 +10,6 @@ public partial class DefaultMedico : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         formatar();
-        selecionarConsulta();
     }
 
     private void formatar()
@@ -23,6 +22,20 @@ public partial class DefaultMedico : System.Web.UI.Page
                 gvConsultas.Rows[i].Cells[3].Text = gvConsultas.Rows[i].Cells[3].Text.Split(' ')[0];
                 // fortatar inicio
                 gvConsultas.Rows[i].Cells[4].Text = gvConsultas.Rows[i].Cells[4].Text.Substring(0, 5);
+
+             /*   if (gvConsultas.SelectedRow.Cells[6].Text == "AGENDADA")
+                {
+                    gvConsultas.SelectedRow.Cells[6].BackColor = System.Drawing.Color.Yellow;
+
+                }
+                else if (gvConsultas.SelectedRow.Cells[6].Text == "OCORRIDA")
+                {
+                    gvConsultas.SelectedRow.Cells[6].BackColor = System.Drawing.Color.DarkGreen;
+                }
+                else if (gvConsultas.SelectedRow.Cells[6].Text == "CANCELADA")
+                {
+                    gvConsultas.SelectedRow.Cells[6].BackColor = System.Drawing.Color.DarkRed;
+                }*/
             }
             catch
             {
